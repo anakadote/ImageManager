05c99d3215ea1e26c3cd9437e214b00bf1f00746
Taylor Collins <taylor@taylorcollinsdesign.com>
2015-01-11 15:52:02 -0500

    Initial commit

---
+++ src/Anakadote/ImageManager/ImageManagerServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Anakadote\Imagemanager;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImageManagerServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('anakadote/imagemanager');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
